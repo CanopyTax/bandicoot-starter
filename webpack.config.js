08ae950ea9b500d57dd4d939f211a02a72fea14c
Devin Rasmussen <devin.rasmussen@canopytax.com>
2020-05-11 21:28:11 -0600

    upgrade all packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,27 @@ const path = require("path");
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
+    publicPath: "bandicoot-starter",
     path: path.resolve(__dirname, "dist"),
-    filename: "bandicoot-starter.js"
+    filename: "bandicoot-starter.js",
   },
   mode: "development",
   devtool: "sourcemap",
   resolve: {
     alias: {
-      react: path.resolve(__dirname, "node_modules/react")
-    }
+      react: path.resolve(__dirname, "node_modules/react"),
+    },
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: "babel-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "src")
-  }
+    contentBase: path.resolve(__dirname, "src"),
+  },
 };
